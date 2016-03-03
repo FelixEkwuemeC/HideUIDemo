7d8cdd632fbed73095a70bc914450703cba7f93e
Felix Ekwueme <felixekwueme@felixs-mbp.attlocal.net>
2016-03-03 16:35:16 -0600

    Initial commit

---
+++ app/src/main/java/com/felixekwueme/hideuidemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.felixekwueme.hideuidemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+
+    public void show(View view) {
+            textView.setVisibility(View.VISIBLE);
+        Log.i("Text Visible", "Complete");
+    }
+
+    public void hide(View view) {
+            textView.setVisibility(View.INVISIBLE);
+
+        Log.i("Text Hidden", "Complete");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.myTextView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
